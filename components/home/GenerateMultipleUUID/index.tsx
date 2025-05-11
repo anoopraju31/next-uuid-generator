9f51e7ef58633749897a274f9e76eab3aa461d84
anoop raju <anoopraju31@gmail.com>
2025-05-11 22:49:56 +0530

    Chore: Multiple UUID Generatro

---
+++ components/home/GenerateMultipleUUID/index.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { useEffect, useState, type FC } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+const GenerateMultipleUUID: FC = () => {
+	const [count, setCount] = useState<number>(0)
+	const [uuids, setUuids] = useState<string[]>([])
+	const [copied, setCopied] = useState<boolean[]>([])
+	const [generateNewUuid, setGenerateNewUuid] = useState(false)
+
+	useEffect(() => {
+		if (count === 0 || count > 500 || !generateNewUuid) return
+
+		const newUuids: string[] = []
+		const newCopied: boolean[] = []
+
+		for (let i = 0; i < count; i++) {
+			const newUuid = uuidv4()
+			newUuids.push(newUuid)
+			newCopied.push(false)
+		}
+
+		setUuids(newUuids)
+		setCopied(newCopied)
+		setGenerateNewUuid(false)
+	}, [count, generateNewUuid])
+
+	const handleCopy = async (index: number) => {
+		try {
+			if (copied[index]) return
+			await navigator.clipboard.writeText(uuids[index])
+			setCopied((prev) => prev.map((item, idx) => (idx === index ? true : item)))
+		} catch (err) {
+			console.error('Failed to copy!', err)
+		}
+	}
+
+	return (
+		<section className='w-full py-8 px-4 sm:px-6 md:px-8 lg:px-12 xl:xp-16 flex flex-col items-center gap-8 bg-black'>
+			<div className='w-full flex justify-center item-center'>
+				<h1 className='text-white text-3xl font-bold'> Generator Multiple V4 UUIDs </h1>
+			</div>
+
+			<div className='w-full flex flex-col gap-6 items-center'>
+				<input
+					className='w-full py-3 px-4 rounded-xl bg-white text-xl font-bold text-black'
+					type='number'
+					placeholder='Enter the number of UUIDs you want to generate'
+					onChange={(e) => setCount(Number(e.target.value))}
+				/>
+				<button
+					onClick={() => setGenerateNewUuid(true)}
+					className='w-fit py-3 px-4 rounded-xl bg-white text-xl font-bold text-black'
+				>
+					Generate
+				</button>
+			</div>
+
+			<div className='w-full grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+				{uuids.map((uuid, index) => (
+					<div key={uuid} className='flex flex-col gap-4 items-center'>
+						<button
+							disabled={copied[index]}
+							onClick={() => handleCopy(index)}
+							className='w-fit py-3 px-4 rounded-xl bg-white text-xl font-bold text-black disabled:opacity-60 disabled:cursor-not-allowed'
+						>
+							{uuid}
+						</button>
+					</div>
+				))}
+			</div>
+		</section>
+	)
+}
+
+export default GenerateMultipleUUID
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,10 +1,12 @@
 import { type FC } from 'react'
 import GenerateSingleUUID from './GenerateSingleUUID'
+import GenerateMultipleUUID from './GenerateMultipleUUID'
 
 const Home: FC = () => {
 	return (
 		<main className='w-ful min-h-screen flex flex-col gap-8 lg:gap-12 font-mono'>
 			<GenerateSingleUUID />
+			<GenerateMultipleUUID />
 		</main>
 	)
 }
