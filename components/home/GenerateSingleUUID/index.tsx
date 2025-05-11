40b2ee484c9e597e49cbc3fc59329bb87b86d40d
anoop raju <anoopraju31@gmail.com>
2025-05-11 22:22:53 +0530

    Chore: Generate Single UUID added

---
+++ components/home/GenerateSingleUUID/index.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useEffect, useState, type FC } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+const GenerateSingleUUID: FC = () => {
+    	const [singleUuid, setSingleUuid] = useState('')
+	const [copied, setCopied] = useState(false)
+	const [generateNewUuid, setGenerateNewUuid] = useState(true)
+
+	useEffect(() => {
+		if (!generateNewUuid) return
+		const newUuid = uuidv4()
+		setSingleUuid(newUuid)
+		setGenerateNewUuid(false)
+	}, [generateNewUuid])
+
+	const generateNewUuidClick = () => {
+		setGenerateNewUuid(true)
+	}
+
+	const handleCopy = async () => {
+		try {
+			await navigator.clipboard.writeText(singleUuid)
+			setCopied(true)
+			setTimeout(() => setCopied(false), 2000) // Reset after 2 seconds
+		} catch (err) {
+			console.error('Failed to copy!', err)
+		}
+	}
+
+	return (
+        			<section className='w-full py-16 px-4 sm:px-6 md:px-8 lg:px-12 xl:xp-16 flex flex-col items-center gap-8 bg-black'>
+				<div className='w-full flex justify-center item-center'>
+					<h1 className='text-white text-3xl font-bold'> Online UUID Generator </h1>
+				</div>
+
+				<div className='w-full flex flex-col gap-6 items-center'>
+					<h2 className='text-2xl font-semibold'>Your Version 4 UUID: </h2>
+
+					<button
+						onClick={handleCopy}
+						className='w-fit py-3 md:py-6 px-4 md:px-6 rounded-xl bg-white text-xl font-bold text-black cursor-pointer'
+					>
+						{' '}
+						{singleUuid}{' '}
+					</button>
+
+					<button
+						onClick={generateNewUuidClick}
+						className='w-fit py-3 md:py-6 px-4 md:px-6 rounded-xl bg-white text-xl font-bold text-black'
+					>
+						Click here to generate another UUID.
+					</button>
+				</div>
+			</section>
+
+    )
+}
+
+export default GenerateSingleUUID
