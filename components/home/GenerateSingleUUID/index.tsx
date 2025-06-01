102abbb424eb94c671499b910cc32cca58a58884
anoop raju <anoopraju31@gmail.com>
2025-06-01 23:29:22 +0530

    Chore: style updated

--- components/home/GenerateSingleUUID/index.tsx
+++ components/home/GenerateSingleUUID/index.tsx
@@ -31,27 +31,29 @@ const GenerateSingleUUID: FC = () => {
 	return (
 		<section className='w-full py-16 px-4 sm:px-6 md:px-8 lg:px-12 xl:xp-16 flex flex-col items-center gap-8 bg-black'>
 			<div className='w-full flex justify-center item-center'>
-				<h1 className='text-white text-3xl font-bold'> Online UUID Generator </h1>
+				<h1 className='text-white text-center text-3xl font-bold'> Online UUID Generator </h1>
 			</div>
 
-			<div className='w-full flex flex-col gap-6 items-center'>
-				<h2 className='text-2xl font-semibold'>Your Version 4 UUID: </h2>
+			<div className='w-full md:w-fit flex flex-col gap-6 items-center'>
+				<h2 className='w-full text-2xl text-left font-semibold'>Your Version 4 UUID: </h2>
 
-				<button
-					onClick={handleCopy}
-					disabled={copied}
-					className='w-fit py-3 md:py-6 px-4 md:px-6 rounded-xl bg-white text-xl font-bold text-black cursor-pointer disabled:opacity-60 disabled:cursor-not-allowed'
-				>
-					{' '}
-					{singleUuid}{' '}
-				</button>
+				<div className='flex flex-col lg:flex-row items-center gap-6'>
+					<button
+						onClick={handleCopy}
+						disabled={copied}
+						className='w-full md:w-fit py-3 md:py-6 px-4 md:px-6 rounded-xl bg-white text-lg md:text-xl font-bold text-black cursor-pointer disabled:opacity-60 disabled:cursor-not-allowed'
+					>
+						{' '}
+						{singleUuid}{' '}
+					</button>
 
-				<button
-					onClick={generateNewUuidClick}
-					className='w-fit py-3 md:py-6 px-4 md:px-6 rounded-xl bg-white text-xl font-bold text-black'
-				>
-					Click here to generate another UUID.
-				</button>
+					<button
+						onClick={generateNewUuidClick}
+						className='w-full md:w-fit py-3 md:py-6 px-4 md:px-6 rounded-xl bg-white text-lg md:text-xl font-bold text-black'
+					>
+						Click here to generate another UUID.
+					</button>
+				</div>
 			</div>
 		</section>
 	)
