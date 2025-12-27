0f32af73735161900e0f0814f3c998c27888eab0
anoop raju <anoopraju31@gmail.com>
2025-12-27 21:58:40 +0530

    CHORE: updated

--- components/home/GenerateMultipleUUID/index.tsx
+++ components/home/GenerateMultipleUUID/index.tsx
@@ -1,17 +1,29 @@
 'use client'
 
-import { useEffect, useState, type FC } from 'react'
+import { FormEvent, useState, type FC } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+
 import UuidContainer from './UuidContainer'
 
 const GenerateMultipleUUID: FC = () => {
 	const [count, setCount] = useState<number>(0)
 	const [uuids, setUuids] = useState<string[]>([])
 	const [copied, setCopied] = useState<boolean[]>([])
-	const [generateNewUuid, setGenerateNewUuid] = useState(false)
 
-	useEffect(() => {
-		if (count === 0 || count > 500 || !generateNewUuid) return
+	const handleCopy = async (index: number) => {
+		try {
+			if (copied[index]) return
+			await navigator.clipboard.writeText(uuids[index])
+			setCopied((prev) => prev.map((item, idx) => (idx === index ? true : item)))
+		} catch (err) {
+			console.error('Failed to copy!', err)
+		}
+	}
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (count <= 0 || count > 500) return
 
 		const newUuids: string[] = []
 		const newCopied: boolean[] = []
@@ -24,17 +36,6 @@ const GenerateMultipleUUID: FC = () => {
 
 		setUuids(newUuids)
 		setCopied(newCopied)
-		setGenerateNewUuid(false)
-	}, [count, generateNewUuid])
-
-	const handleCopy = async (index: number) => {
-		try {
-			if (copied[index]) return
-			await navigator.clipboard.writeText(uuids[index])
-			setCopied((prev) => prev.map((item, idx) => (idx === index ? true : item)))
-		} catch (err) {
-			console.error('Failed to copy!', err)
-		}
 	}
 
 	return (
@@ -43,7 +44,7 @@ const GenerateMultipleUUID: FC = () => {
 				<h1 className='text-white text-center text-3xl font-bold'> Generator Multiple V4 UUIDs </h1>
 			</div>
 
-			<div className='w-full flex flex-col sm:flex-row gap-6 items-end'>
+			<form onSubmit={handleSubmit} className='w-full flex flex-col sm:flex-row gap-6 items-end'>
 				<div className='w-full flex flex-col gap-3'>
 					<label htmlFor='count' className='text-white text-lg md:text-xl font-bold'>
 						Enter the number of UUIDs you want to generate
@@ -60,12 +61,12 @@ const GenerateMultipleUUID: FC = () => {
 				</div>
 
 				<button
-					onClick={() => setGenerateNewUuid(true)}
+					type='submit'
 					className='w-full sm:w-fit py-3 px-4 rounded-xl bg-white text-xl font-bold text-black'
 				>
 					Generate
 				</button>
-			</div>
+			</form>
 
 			<UuidContainer uuids={uuids} copied={copied} handleCopy={handleCopy} />
 		</section>
